0a99d3fec4af1238027fdc7ca69405e39e27f28f
fk2019 <fkmuiruri8@gmail.com>
2023-12-07 11:24:00 -0800

    Add host to express server

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const routes = require('./routes/index');
 
 const port = process.env.PORT || 5000;
+const host = '127.0.0.1';
 const app = express();
 
 app.use('/', routes);
 
-app.listen(port, () => {
+app.listen(host, port, () => {
   console.log(`Server running on port ${port}`);
 });
 export default app;
