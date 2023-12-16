d6fab4c00be9af1cce7d4d41e6f41be4ad64f2f7
fk2019 <fkmuiruri8@gmail.com>
2023-12-16 12:24:14 -0800

    Add json response

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,10 +5,10 @@ const crypto = require('crypto');
 class UsersController {
     static async postNew(request, response) {
         const { email, password } = request.body;
-        if (!email) return response.status(400).send('Missing email');
-        if (!password) return response.status(400).send('Missing password');
+        if (!email) return response.status(400).json({ error: 'Missing email' });
+        if (!password) return response.status(400).json({ error: 'Missing password' });
         const user = await dbClient.findUser({ email });
-        if (user) return response.status(400).send('Already exists');
+	if (user) return response.status(400).json({ error: 'Already exist' });
         const hashPass = crypto.createHash('sha1').update(password).digest('hex');
         const newUser = await dbClient.addUser(email, hashPass);
         return response.status(200).json({ id: newUser._id, email: newUser.email });
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const appController = require('../controllers/AppController');
+
 const usersController = require('../controllers/UsersController');
 
 router.get('/status', appController.getStatus);
